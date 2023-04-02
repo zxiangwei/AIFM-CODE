0488be043def4c720389380c3a4c9c5fe27bb51c
zxiangwei <44313751+zxiangwei@users.noreply.github.com>
2023-04-02 20:03:48 +0800

    Update build_all.sh

--- build_all.sh
+++ build_all.sh
@@ -2,10 +2,10 @@
 
 # Shenango
 cd shenango
-sudo ./build.sh || { echo 'Failed to build Shenango.'; exit 1; }
+./build.sh || { echo 'Failed to build Shenango.'; exit 1; }
 cd ..
 
 # AIFM
 cd aifm
-sudo ./build.sh || { echo 'Failed to build AIFM.'; exit 1; }
+./build.sh || { echo 'Failed to build AIFM.'; exit 1; }
 cd ..
